8205386ef6a1d668a762696b854487c6768888c7
Nicola Szabo <nicola.szabo@stud.unibas.ch>
2024-10-21 10:00:13 +0200

    latest update

--- segmentation_heart.py
+++ segmentation_heart.py
@@ -23,11 +23,17 @@ def segment_images(input_directory, output_directory, object_of_interest):
     for filename in os.listdir(input_directory):
         if filename.endswith('.nii.gz'):
             input_path = os.path.join(input_directory, filename)
-            output_path = os.path.join(output_directory, os.path.splitext(filename)[0])
+
+            # Extract the first 6 characters from the filename (xx-xxxx)
+            base_filename = filename[:7]  # Extract the first 7 characters, which includes 'xx-xxxx'
+            
+            # Create a descriptive filename by appending the object of interest to the base filename
+            custom_filename = f"{base_filename}_{'_'.join(object_of_interest)}.nii.gz"  # Add the objects of interest
+            output_path = os.path.join(output_directory, custom_filename)
             
             # Run TotalSegmentator for each NIfTI file
-            print(f'Processing file: {input_path}')
-            python_api.totalsegmentator(input_path, output_path, task = 'total', roi_subset = object_of_interest) # without GPU use: fastest = True
+            print(f'Processing file: {input_path} -> Output: {output_path}')
+            python_api.totalsegmentator(input_path, output_path, task = 'total', roi_subset = object_of_interest)
                                                                                                                                                 
                                                                                                                                                   
             
@@ -37,8 +43,8 @@ if __name__ == '__main__':
     #Check if GPU is available
     print(torch.cuda.is_available())
     
-    input_directory = 'NIFTI_files/'
-    output_directory = 'masks_heart/' 
+    input_directory = 'G:/data/unhealthy_nifti/'
+    output_directory = 'G:/data/unhealthy_segmentation' 
     object_of_interest = ['heart', 'aorta'] 
     
     segment_images(input_directory, output_directory, object_of_interest) 
