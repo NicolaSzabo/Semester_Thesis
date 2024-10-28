a5c344de18dbfcfd1c4bff48cb258e061bfc5c8f
Nicola Szabo <nicolaszabo@MacBook-Pro-von-Nicola.local>
2024-10-28 09:14:20 +0100

    classification MONAI

--- classification.py
+++ classification.py
@@ -37,7 +37,9 @@ set_determinism(seed = 0)
 
 ### Read image filenames from the dataset folders
 
-data_dir = '/home/fit_member/Documents/NS_SemesterWork/data/data_classification'
+# for Mac:  '/Users/nicolaszabo/Library/CloudStorage/OneDrive-Persönlich/Desktop/Semester_Thesis/Project/data/data_classification'
+# for Linux: '/home/fit_member/Documents/NS_SemesterWork/data/data_classification'
+data_dir = '/Users/nicolaszabo/Library/CloudStorage/OneDrive-Persönlich/Desktop/Semester_Thesis/Project/data/data_classification'
 
 class_names = sorted(x for x in os.listdir(data_dir) if os.path.isdir(os.path.join(data_dir, x)))
 num_class = len(class_names)
@@ -67,7 +69,7 @@ print(f"Label counts: {num_each}")
 
 
 ### Randomly pick images from the dataset to visualize and check
-
+"""
 plt.subplots(3, 3, figsize = (8, 8))
 for i, k in enumerate(np.random.randint(num_total, size = 9)):
     im = nib.load(image_files_list[k])
@@ -78,6 +80,199 @@ for i, k in enumerate(np.random.randint(num_total, size = 9)):
     plt.imshow(arr, cmap = 'gray')
 plt.tight_layout()
 plt.show()
+"""
+
 
 
 ### Prepare training, validation and test data lists
+val_frac = 0.1
+test_frac = 0.1
+length = len(image_files_list)
+indices = np.arange(length)
+np.random.shuffle(indices)
+
+test_split = int(length * test_frac)
+val_split = int(val_frac * length) + test_split
+test_indices = indices[:test_split]
+val_indices = indices[test_split:val_split]
+train_indices = indices[val_split:]
+
+train_x = [image_files_list[i] for i in train_indices]
+train_y = [image_class[i] for i in train_indices]
+val_x = [image_files_list[i] for i in val_indices]
+val_y = [image_class[i] for i in val_indices]
+test_x = [image_files_list[i] for i in test_indices]
+test_y = [image_class[i] for i in test_indices]
+
+print(f"Training count: {len(train_x)}, Validation count: {len(val_x)}, Test count: {len(test_x)}")
+
+
+
+
+
+
+
+### Define the MONAI transforms for data preprocessing and augmentation
+train_transforms = Compose(
+    [
+        LoadImage(image_only = True),
+        EnsureChannelFirst(),
+        ScaleIntensity(),
+        RandRotate(range_x = np.pi / 12, prob = 0.5, keep_size = True),
+        RandFlip(spatial_axis = 0, prob = 0.5),
+        RandZoom(min_zoom = 0.9, max_zoom = 1.1, prob = 0.5),
+    ]
+)
+
+val_transforms = Compose([LoadImage(image_only = True), EnsureChannelFirst(), ScaleIntensity()])
+
+y_pred_trans = Compose([Activations(softmax = True)])
+y_trans = Compose(AsDiscrete(to_onehot = num_class))
+
+
+class HeartClassification(torch.utils.data.Dataset):
+    def __init__(self, image_files, labels, transforms):
+        self.image_files = image_files
+        self.labels = labels
+        self.transforms = transforms
+
+    def __len__(self):
+        return len(self.image_files)
+
+    def __getitem__(self, index):
+        return self.transforms(self.image_files[index]), self.labels[index]
+
+
+train_ds = HeartClassification(train_x, train_y, train_transforms)
+train_loader = DataLoader(train_ds, batch_size = 300, shuffle = True, num_workers = 2)
+
+val_ds = HeartClassification(val_x, val_y, val_transforms)
+val_loader = DataLoader(val_ds, batch_size = 300, num_workers = 2)
+
+test_ds = HeartClassification(test_x, test_y, val_transforms)
+test_loader = DataLoader(test_ds, batch_size = 300, num_workers = 2)
+
+
+
+
+
+### Define network and optimizer
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model = DenseNet121(spatial_dims = 2, in_channels = 1, out_channels = num_class).to(device)
+loss_function = torch.nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(model.parameters(), 1e-5)
+max_epochs = 4
+val_interval = 1
+auc_metric = ROCAUCMetric()
+
+
+
+
+
+
+### Model training
+best_metric = -1
+best_metric_epoch = -1
+epoch_loss_values = []
+metric_values = []
+writer = SummaryWriter()
+
+for epoch in range(max_epochs):
+    print("-" * 10)
+    print(f"epoch {epoch + 1}/{max_epochs}")
+    model.train()
+    epoch_loss = 0
+    step = 0
+    for batch_data in train_loader:
+        step += 1
+        inputs, labels = batch_data[0].to(device), batch_data[1].to(device)
+        optimizer.zero_grad()
+        outputs = model(inputs)
+        loss = loss_function(outputs, labels)
+        loss.backward()
+        optimizer.step()
+        epoch_loss += loss.item()
+        print(f"{step}/{len(train_ds) // train_loader.batch_size}, " f"train_loss: {loss.item():.4f}")
+        epoch_len = len(train_ds) // train_loader.batch_size
+        writer.add_scalar("train_loss", loss.item(), epoch_len * epoch + step)
+    epoch_loss /= step
+    epoch_loss_values.append(epoch_loss)
+    print(f"epoch {epoch + 1} average loss: {epoch_loss:.4f}")
+
+    if (epoch + 1) % val_interval == 0:
+        model.eval()
+        with torch.no_grad():
+            y_pred = torch.tensor([], dtype=torch.float32, device=device)
+            y = torch.tensor([], dtype=torch.long, device=device)
+            for val_data in val_loader:
+                val_images, val_labels = (
+                    val_data[0].to(device),
+                    val_data[1].to(device),
+                )
+                y_pred = torch.cat([y_pred, model(val_images)], dim=0)
+                y = torch.cat([y, val_labels], dim=0)
+            y_onehot = [y_trans(i) for i in decollate_batch(y, detach=False)]
+            y_pred_act = [y_pred_trans(i) for i in decollate_batch(y_pred)]
+            auc_metric(y_pred_act, y_onehot)
+            result = auc_metric.aggregate()
+            auc_metric.reset()
+            del y_pred_act, y_onehot
+            metric_values.append(result)
+            acc_value = torch.eq(y_pred.argmax(dim=1), y)
+            acc_metric = acc_value.sum().item() / len(acc_value)
+            if result > best_metric:
+                best_metric = result
+                best_metric_epoch = epoch + 1
+                torch.save(model.state_dict(), os.path.join(root_dir, "best_metric_model.pth"))
+                print("saved new best metric model")
+            print(
+                f"current epoch: {epoch + 1} current AUC: {result:.4f}"
+                f" current accuracy: {acc_metric:.4f}"
+                f" best AUC: {best_metric:.4f}"
+                f" at epoch: {best_metric_epoch}"
+            )
+            writer.add_scalar("val_accuracy", acc_metric, epoch + 1)
+
+print(f"train completed, best_metric: {best_metric:.4f} " f"at epoch: {best_metric_epoch}")
+writer.close()
+
+
+
+
+
+### Plot the loss and metric values
+plt.figure("train", (12, 6))
+plt.subplot(1, 2, 1)
+plt.title("Epoch Average Loss")
+x = [i + 1 for i in range(len(epoch_loss_values))]
+y = epoch_loss_values
+plt.xlabel("epoch")
+plt.plot(x, y)
+plt.subplot(1, 2, 2)
+plt.title("Val AUC")
+x = [val_interval * (i + 1) for i in range(len(metric_values))]
+y = metric_values
+plt.xlabel("epoch")
+plt.plot(x, y)
+plt.show()
+
+
+
+
+### Evaluate the model on the test dataset
+model.load_state_dict(torch.load(os.path.join(data_dir, "heart_classification.pth")))
+model.eval()
+y_true = []
+y_pred = []
+with torch.no_grad():
+    for test_data in test_loader:
+        test_images, test_labels = (
+            test_data[0].to(device),
+            test_data[1].to(device),
+        )
+        pred = model(test_images).argmax(dim=1)
+        for i in range(len(pred)):
+            y_true.append(test_labels[i].item())
+            y_pred.append(pred[i].item())
+
+print(classification_report(y_true, y_pred, target_names=class_names, digits=4))            
